5fa4ae1273e5719dc09f03fe6ef9f4cd9c3d0bca
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2024-01-08 22:27:51 +0200

    Create sample tests

---
+++ app/core/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Adds two numbers together and returns the result
+
+    Args:
+        x (number): first number
+        y (number): second number
+    """
+    return x + y
